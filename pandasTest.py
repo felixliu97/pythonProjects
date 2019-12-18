bda7088e394d469d8b452ad041dbb31a1af2485e
Felix Liu <liusuqing97@gmail.com>
2019-12-18 22:25:28 +1100

    initial files

---
+++ pandasTest.py
@@ -0,0 +1,6 @@
+import pandas as pd
+
+data = [1,2,3,4]
+series1 = pd.Series(data)
+print(series1)
+print(type(series1))
---
+++ test.py
@@ -0,0 +1,2 @@
+square_list = [0, 1, 4, 9, 16, 25]
+print(square_list[1:-2])
