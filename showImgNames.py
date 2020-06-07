875b61aa64d697503b3910c25136bd58f834aa98
Felix Liu <felix.liu@arq.group>
2020-06-07 10:45:03 +1000

    changes

--- showImgNames.py
+++ showImgNames.py
@@ -1,5 +1,5 @@
 import os, glob
 
-os.chdir(r"C:\Users\liusu\Desktop\photos\wedding photos")
-for file in glob.glob("*.jpg"):
+os.chdir(r"C:\Users\felix.liu\Desktop\music")
+for file in glob.glob("*.mp3"):
     print(file)
