02ae627925cace909514ef75bdfc726333e682a8
Felix Liu <liusuqing97@gmail.com>
2021-03-12 15:12:36 +1100

    remove white space

--- CodingProblem/prime_number.py
+++ CodingProblem/prime_number.py
@@ -4,7 +4,7 @@ number = int(input('Input: '))
 assert(number > 2 and number % 2 == 0)
 
 prime_numbers = []
-  
+
 for num in range(2, number - 1):
     if all(num % i != 0 for i in range(2, int(math.sqrt(num)) + 1)):
         prime_numbers.append(num)
