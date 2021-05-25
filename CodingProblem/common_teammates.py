444d5c4dcad304efba455c72b17619a7d2b5c7ee
Felix Liu <fliu@mla.com.au>
2021-05-25 12:03:11 +1000

    change

--- CodingProblem/common_teammates.py
+++ CodingProblem/common_teammates.py
@@ -1,7 +1,5 @@
 from bs4 import BeautifulSoup
 import requests
-import pandas as pd
-from requests.models import Response
 
 headers = {'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.87 Safari/537.36'}
 
