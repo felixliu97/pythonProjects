7eae91fd3686a7fbb5dca7e5477152dcb82caa2e
Felix Liu <fliu@mla.com.au>
2022-03-07 20:39:01 +1100

    changes

--- CodingProblem/nba_common_teammates.py
+++ CodingProblem/nba_common_teammates.py
@@ -6,15 +6,15 @@ headers = {'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleW
 player_teammates_url = {
     # 'Kobe-Bryant':'https://basketball.realgm.com/player/Kobe-Bryant/Teammates/613',
     # 'Derrick-Rose':'https://basketball.realgm.com/player/Derrick-Rose/Teammates/756',
-    # 'Giannis-Antetokounmpo':'https://basketball.realgm.com/player/Giannis-Antetokounmpo/Teammates/49629',
+    'Giannis-Antetokounmpo':'https://basketball.realgm.com/player/Giannis-Antetokounmpo/Teammates/49629',
     # 'Dwight-Howard':'https://basketball.realgm.com/player/Dwight-Howard/Teammates/376',
     # 'Kyle-Lowry':'https://basketball.realgm.com/player/Kyle-Lowry/Teammates/78',
     # 'Kawhi-Leonard':'https://basketball.realgm.com/player/Kawhi-Leonard/Teammates/2256',
-    'LeBron-James':'https://basketball.realgm.com/player/LeBron-James/Teammates/250',
-    'Dwyane-Wade':'https://basketball.realgm.com/player/Dwyane-Wade/Teammates/450',
+    # 'LeBron-James':'https://basketball.realgm.com/player/LeBron-James/Teammates/250',
+    # 'Dwyane-Wade':'https://basketball.realgm.com/player/Dwyane-Wade/Teammates/450',
     # 'Kevin-Durant':'https://basketball.realgm.com/player/Kevin-Durant/Teammates/34',
     # 'Kevin-Garnett':'https://basketball.realgm.com/player/Kevin-Garnett/Teammates/644',
-    'Chris-Paul':'https://basketball.realgm.com/player/Chris-Paul/Teammates/61',
+    # 'Chris-Paul':'https://basketball.realgm.com/player/Chris-Paul/Teammates/61',
     # 'Kyrie-Irving':'https://basketball.realgm.com/player/Kyrie-Irving/Teammates/7118',
     # 'Rajon-Rondo':'https://basketball.realgm.com/player/Rajon-Rondo/Teammates/93',
     # 'JR-Smith':'https://basketball.realgm.com/player/JR-Smith/Teammates/386',
@@ -23,12 +23,14 @@ player_teammates_url = {
     # 'Allen-Iverson':'https://basketball.realgm.com/player/Allen-Iverson/Teammates/603',
     # 'Shaquille-ONeal':'https://basketball.realgm.com/player/Shaquille-ONeal/Teammates/755',
     # 'Trevor-Ariza':'https://basketball.realgm.com/player/Trevor-Ariza/Teammates/398',
-    # 'James-Harden':'https://basketball.realgm.com/player/James-Harden/Teammates/1598',
+    'James-Harden':'https://basketball.realgm.com/player/James-Harden/Teammates/1598',
     # 'Stephen-Curry':'https://basketball.realgm.com/player/Stephen-Curry/Teammates/1600',
     # 'Russell-Westbrook':'https://basketball.realgm.com/player/Russell-Westbrook/Teammates/759',
     # 'Anthony-Davis':'https://basketball.realgm.com/player/Anthony-Davis/Teammates/13305',
     # 'Jimmy-Butler':'https://basketball.realgm.com/player/Jimmy-Butler/Teammates/6160',
-    'Carmelo-Anthony':'https://basketball.realgm.com/player/Carmelo-Anthony/Teammates/452'
+    # 'Carmelo-Anthony':'https://basketball.realgm.com/player/Carmelo-Anthony/Teammates/452',
+    # 'Paul-Millsap':'https://basketball.realgm.com/player/Paul-Millsap/Teammates/112',
+    'Nikola-Jokic':'https://basketball.realgm.com/player/Nikola-Jokic/Teammates/49571'
 }
 
 greens_teammates_url = {
@@ -49,9 +51,7 @@ sixth_man_teammates_url = {
 
 common_teammates = []
 
-# for _, url in player_teammates_url.items():
-# for _, url in greens_teammates_url.items():
-for _, url in sixth_man_teammates_url.items():
+for _, url in player_teammates_url.items():
     response = requests.get(url, headers=headers)
     page = BeautifulSoup(response.text, 'html.parser')
     teammates_table = page.find('tbody')
@@ -67,4 +67,4 @@ for _, url in sixth_man_teammates_url.items():
     else:
         common_teammates = set(common_teammates).intersection(teammates_list)
 
-print(common_teammates)
+print(f"common_teammates: {common_teammates}")
--- testing/testing.py
+++ testing/testing.py
@@ -4,16 +4,24 @@ import re
 import datetime
 
 def transform(fusion):
-    values = values = re.findall(r'^.* (\d+/\d+/\d+) (\w+)\((\d+):(\d+)\)$', fusion)
-    return ' '.join(values[0])
+    values = re.findall(r'^.* (\d+/\d+/\d+) (\w+)\((\d+):(\d+)\)$', fusion)
+    print(values)
+    return '|'.join(values[0])
 
 df = pd.DataFrame({'fusion': ['WC 7/01/2020 Metro(1:2020)']})
-df[['a','b','c','d']] = df['fusion'].apply(transform).str.split(expand=True)
+df[['a','b','c','d']] = df['fusion'].apply(transform).str.split('|',expand=True)
 df['e'] = "1,000,000"
 df['e'] = df['e'].str.replace(',', '')
 df['f'] = '2.5%'
 df['f'] = df['f'].astype(str).str.replace('%', '')
 df['a'] = pd.to_datetime(df['a'])
 df['g'] = '"' + df['f'] + '"'
-df.drop(['fusion'], axis=1, inplace=True)
+if 'fusion1' in df:
+    df.drop(['fusion1'], axis=1, inplace=True)
 print(f"df:{df.head(5)}")
+
+
+s = "s3://bucket/folder/file (ghi abc). CSV"
+ret = re.findall(r'\((.*)\)', s)
+str = ret[0] if ret else ''
+print(str)
