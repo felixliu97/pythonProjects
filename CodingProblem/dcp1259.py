ae2eeb8016ff1a2ae3bc88e483da038bce56152f
Felix Liu <liusuqing97@gmail.com>
2022-12-02 12:43:07 +1100

    updates

--- CodingProblem/dcp1259.py
+++ CodingProblem/dcp1259.py
@@ -2,10 +2,6 @@ def count_flip(s:str):
     x = s.rstrip('y')
     return x.count('y')
 
-test1 = 'xyxxxyxyy'
-test2 = 'xxxxxyxyy'
-test3 = 'xxyyyx'
-
 tests = {
     'xyxxxyxyy' : 2
     ,'xxxxxyxyy' : 1
