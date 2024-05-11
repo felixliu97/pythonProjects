e550b342c9d75cd7792810f172447fa900de84cc
Felix Liu <liusuqing97@gmail.com>
2024-05-11 19:19:35 +1000

    test

--- testing/test.py
+++ testing/test.py
@@ -1,8 +1,13 @@
-a = None
-b = 1
-c = ''
+import os
 
-if c == 2:
-    print("a OK!")
-else:
-    print("a not OK!")
+in_path = os.path.join(os.getcwd(), "input.txt")
+output_path = os.path.join(os.getcwd(), "output.txt")
+
+with open(in_path, "r") as file:
+    ids = file.read().splitlines()
+
+sorted_ids = sorted(ids)
+deduplicated_ids = list(dict.fromkeys(sorted_ids))
+
+with open(output_path, "w") as file:
+    file.write("\n".join(deduplicated_ids))
