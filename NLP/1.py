2676ccd871a04ec3dfd8b72f66fa55f2b59aecb5
Felix Liu <liusuqing97@gmail.com>
2024-07-28 20:34:43 +1000

    test

--- NLP/1.py
+++ NLP/1.py
@@ -1,10 +1,17 @@
 import nltk
+from nltk.corpus import treebank
+t = treebank.parsed_sents('wsj_0001.mrg')[0]
 
 sentence = """At eight o'clock on Thursday morning
 ... Arthur didn't feel very good."""
 
 tokens = nltk.word_tokenize(sentence)
-print(tokens)
+print(f"tokens:{tokens}")
 
 tagged = nltk.pos_tag(tokens)
-print(tagged[0:6])
+print(f"tagged[0:6]:{tagged[0:6]}")
+
+entities = nltk.chunk.ne_chunk(tagged)
+print(f"entities:{entities}")
+
+t.draw()
