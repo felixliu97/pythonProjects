33a33174568984743f88894459d8a8e0598a91e2
Felix Liu <liusuqing97@gmail.com>
2025-01-05 19:35:27 +1100

    lc

--- Leetcode/2.add-two-numbers.py
+++ Leetcode/2.add-two-numbers.py
@@ -12,14 +12,19 @@
 #         self.next = next
 class Solution:
     def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
-        res = l1.val + l2.val
-        digit, carry = res % 10, res // 10
-        answer = ListNode(digit)
-        if any((l1.next, l2.next, carry)):
-            l1 = l1.next if l1.next else ListNode(0)
-            l2 = l2.next if l2.next else ListNode(0)
-            l1.val += carry
-            answer.next = self.addTwoNumbers(l1, l2)    
-        return answer
+        current = head = ListNode(0)
+        carry = 0
+
+        while l1 or l2 or carry:
+            x = l1.val if l1 else 0
+            y = l2.val if l2 else 0
+            sum = x + y + carry
+            carry = sum // 10
+            current.next = ListNode(sum % 10)
+            current = current.next
+            l1 = l1.next if l1 else None
+            l2 = l2.next if l2 else None
+        return head.next
+
 # @lc code=end
 
--- Leetcode/3.longest-substring-without-repeating-characters.py
+++ Leetcode/3.longest-substring-without-repeating-characters.py
@@ -7,17 +7,13 @@
 # @lc code=start
 class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
-        # use sliding window
-        left = 0
         seen = {}
-        output = 0
-        
-        for right, curr in enumerate(s):
-            if curr in seen:
-                left = max(left, seen[curr] + 1)
-            output = max(output, right - left + 1)
-            seen[curr] = right
-        return output
-        
+        left = max_len = 0
+        for right, cur in enumerate(s):
+            if cur in seen:
+                left = max(left, seen[cur] + 1)
+            max_len = max(max_len, right - left + 1)
+            seen[cur] = right
+        return max_len
 # @lc code=end
 
